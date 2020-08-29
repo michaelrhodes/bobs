b3cbeb7138796384408db4a150b82545e0deb955
michaelrhodes <spam@michaelrhod.es>
2020-08-29 12:31:14 +1000

    Use workbench

--- 1.0.0/benchmark.js
+++ 1.0.0/benchmark.js
@@ -1,2 +1,2 @@
 importScripts('https://pkg.mkr.sx/bobs/' + (version = '1.0.0') + '.js')
-importScripts('https://pkg.mkr.sx/bobs/benchmark.worker.js')
+importScripts('https://pkg.mkr.sx/bobs/benchmark.js')
--- benchmark.js
+++ benchmark.js
@@ -1,31 +1,36 @@
-worker = new Worker('benchmark.worker.js')
-worker.onmessage = onmessage
+importScripts('https://pkg.mkr.sx/workbench/1.0.0/suite.js')
+importScripts('https://pkg.mkr.sx/bencode/2.0.0.js')
 
-hash = '#', check = '✓', messages = []
-results = document.createElement('pre')
-button = document.createElement('button')
-button.textContent = text = 'run benchmark'
-button.onclick = function () {
-  button.disabled = true
-  button.textContent = 'running...'
-  worker.postMessage('run')
+output = bencode.encode(input = {
+  string: 'ẕ̢͒͛͗ͪ̓ḁ̢͑͆̌̍̎ͨ͘l̶̜̖͍̞ͨͫ͑̽̀̑̓͂͞g̷͖̜̤̬͖ͪo͑͌͐̐̐̌̍ͣ̀͏̥̻̹',
+  positive: 42,
+  negative: -42,
+  array: [123, { some: 'thing' }],
+  typed: new Uint8Array([4, 5, 6])
+})
+
+suite('encode', function (run) {
+  run('bencode@2.0.0', bencodeEncode)
+  run('bobs@' + version, bobsEncode)
+})
+
+suite('decode', function (run) {
+  run('bencode@2.0.0', bencodeDecode)
+  run('bobs@' + version, bobsDecode)
+})
+
+function bencodeEncode () {
+  bencode.encode(input)
 }
 
-document.body.appendChild(button)
-document.body.appendChild(results)
+function bobsEncode () {
+  bobs.encode(input)
+}
 
-function onmessage (e) {
-  e.data[0] === hash ||
-  e.data[0] === check ?
-    messages.push('\n' + e.data) :
-    messages.push(e.data)
+function bencodeDecode () {
+  bencode.decode(output)
+}
 
-  results.textContent = (
-    messages.join('\n').trim()
-  )
-
-  if (e.data[0] === check) {
-    button.disabled = false
-    button.textContent = text
-  }
+function bobsDecode () {
+  bobs.decode(output)
 }
